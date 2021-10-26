636e16dbab84d01b94f9e068d592182978bf57ce
Laurentiu Vasile Cretu <37586514+laurentiucretu68@users.noreply.github.com>
2021-10-26 17:38:22 +0300

    Digital Banking App v1.2

--- includes/User.h
+++ includes/User.h
@@ -1,5 +1,8 @@
+#pragma once
 #include <iostream>
+#include <fstream>
 #include <random>
+#include <utility>
 #include <vector>
 #include "Tranzactie.h"
 
@@ -18,129 +21,57 @@ public:
 
     User()= default;;
 
-    User(const User& copie){
-        this->nume_prenume = copie.nume_prenume;
-        this->parola = copie.parola;
-        this->email = copie.email;
-        this->telefon = copie.telefon;
-        this->IBAN = copie.IBAN;
-        this->exp_date = copie.exp_date;
-        this->CIV = copie.CIV;
-        this->suma = copie.suma;
-        this->transaction_history = copie.transaction_history;
-    }
+    User(const User& copie);
 
-    User& operator=(const User& copie){
-        this->nume_prenume = copie.nume_prenume;
-        this->parola = copie.parola;
-        this->email = copie.email;
-        this->telefon = copie.telefon;
-        this->IBAN = copie.IBAN;
-        this->exp_date = copie.exp_date;
-        this->CIV = copie.CIV;
-        this->suma = copie.suma;
-        this->transaction_history = copie.transaction_history;
-        return *this;
-    }
+    User& operator=(const User& copie);
 
-    ~User(){  }
+    ~User()= default;
 
-    User(const std::string &numePrenume, const std::string &parola, const std::string &email,
-         const std::string &telefon, const std::string &iban, const std::pair<unsigned short, unsigned short> &expDate,
-         const std::vector<Tranzactie> &transactionHistory, int civ, int suma) : nume_prenume(numePrenume), parola(parola),
-                                                                       email(email), telefon(telefon), IBAN(iban),
-                                                                       exp_date(expDate),
-                                                                       transaction_history(transactionHistory),
+    User(std::string numePrenume, std::string parola, std::string email,
+         std::string telefon, std::string iban, std::pair<unsigned short, unsigned short> expDate,
+         std::vector<Tranzactie> transactionHistory, int civ, int suma) : nume_prenume(std::move(numePrenume)), parola(std::move(parola)),
+                                                                       email(std::move(email)), telefon(std::move(telefon)), IBAN(std::move(iban)),
+                                                                       exp_date(std::move(expDate)),
+                                                                       transaction_history(std::move(transactionHistory)),
                                                                        CIV(civ), suma(suma) {}
 
-    friend std::ostream &operator<<(std::ostream &os, const User &user) {
-        os << "nume_prenume: " << user.nume_prenume << " parola: " << user.parola << " email: " << user.email
-           << " telefon: " << user.telefon << " IBAN: " << user.IBAN << " exp_date: " << user.exp_date.first<<"/"<<user.exp_date.second
-           << " CIV: " << user.CIV <<" suma: "<<user.suma;
-        return os;
-    }
+    friend std::ostream &operator<<(std::ostream &os, const User &user);
 
-    static void writeUserInFile(const User& copie){
-        std::fstream write;
+    static void writeUserInFile(const User& copie);
 
-        write.open("../txt_files/User/users.txt",std::fstream::app);
-        write<<copie.nume_prenume<<';'<<copie.email<<';'<<copie.parola<<';';
-        write<<copie.telefon<<';'<<copie.IBAN<<';'<<copie.exp_date.first<<'/'<<copie.exp_date.second<<';';
-        write<<copie.CIV<<';'<<copie.suma<<'\n';
+    const std::string &getNumePrenume() const;
 
-        write.close();
-        write.open("../txt_files/User/users.txt",std::fstream::app);
-        write.close();
-    }
+    void setNumePrenume(const std::string &numePrenume);
 
-    const std::string &getNumePrenume() const {
-        return nume_prenume;
-    }
+    const std::string &getParola() const;
 
-    void setNumePrenume(const std::string &numePrenume) {
-        nume_prenume = numePrenume;
-    }
+    void setParola(const std::string &parola_copie);
 
-    const std::string &getParola() const {
-        return parola;
-    }
+    const std::string &getEmail() const;
 
-    void setParola(const std::string &parola_copie) {
-        User::parola = parola_copie;
-    }
+    void setEmail(const std::string &email_copie);
 
-    const std::string &getEmail() const {
-        return email;
-    }
+    const std::string &getTelefon() const;
 
-    void setEmail(const std::string &email_copie) {
-        User::email = email_copie;
-    }
+    void setTelefon(const std::string &telefon_copie);
 
-    const std::string &getTelefon() const {
-        return telefon;
-    }
+    const std::string &getIban() const;
 
-    void setTelefon(const std::string &telefon_copie) {
-        User::telefon = telefon_copie;
-    }
+    void setIban(const std::string &iban);
 
-    const std::string &getIban() const {
-        return IBAN;
-    }
+    const std::pair<unsigned short, unsigned short> &getExpDate() const;
 
-    void setIban(const std::string &iban) {
-        IBAN = iban;
-    }
+    void setExpDate(const std::pair<unsigned short, unsigned short> &expDate);
 
-    const std::pair<unsigned short, unsigned short> &getExpDate() const {
-        return exp_date;
-    }
+    const std::vector<Tranzactie> &getTransactionHistory() const;
 
-    void setExpDate(const std::pair<unsigned short, unsigned short> &expDate) {
-        exp_date = expDate;
-    }
+    int getSuma() const;
 
-    const std::vector<Tranzactie> &getTransactionHistory() const {
-        return transaction_history;
-    }
+    void setSuma(int suma_copie);
 
+    int getCiv() const;
 
-    int getSuma() const {
-        return suma;
-    }
-
-    void setSuma(int suma_copie) {
-        User::suma = suma_copie;
-    }
-
-    int getCiv() const {
-        return CIV;
-    }
-
-    void setCiv(int civ) {
-        CIV = civ;
-    }
+    void setCiv(int civ);
 
     static void changePassword(User& user);
 
