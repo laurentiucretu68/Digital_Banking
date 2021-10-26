4b7e241e27249c9b607414b5b66949fc97ce0102
Laurentiu Vasile Cretu <37586514+laurentiucretu68@users.noreply.github.com>
2021-10-26 17:38:57 +0300

    Digital Banking App v1.2

--- includes/Admin.h
+++ includes/Admin.h
@@ -1,3 +1,10 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <utility>
+#include <vector>
+#include "Tranzactie.h"
 #include "User.h"
 
 class Admin{
@@ -8,40 +15,19 @@ class Admin{
 public:
     Admin()= default;;
 
+    Admin(const Admin& copie);
 
-    Admin(const Admin& copie){
-        this->username = copie.username;
-        this->email = copie.email;
-        this->parola = copie.parola;
-    }
+    Admin& operator=(const Admin& copie);
 
-    Admin& operator=(const Admin& copie){
-        this->username = copie.username;
-        this->email = copie.email;
-        this->parola = copie.parola;
-        return *this;
-    }
+    friend std::ostream &operator<<(std::ostream &os, const Admin &admin);
 
-    friend std::ostream &operator<<(std::ostream &os, const Admin &admin) {
-        os << "username: " << admin.username << " email: " << admin.email << " parola: " << admin.parola;
-        return os;
-    }
+    const std::string &getUsername() const;
 
-    const std::string &getUsername() const {
-        return username;
-    }
+    void setUsername(const std::string &username_copie);
 
-    void setUsername(const std::string &username_copie) {
-        Admin::username = username_copie;
-    }
+    void setEmail(const std::string &email_copie);
 
-    void setEmail(const std::string &email_copie) {
-        Admin::email = email_copie;
-    }
-
-    void setParola(const std::string &parola_copie) {
-        Admin::parola = parola_copie;
-    }
+    void setParola(const std::string &parola_copie);
 
     static void showAllUsers(const Admin&, unsigned short&);
 
